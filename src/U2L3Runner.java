d8dbd2f900c6594bcd508bf535f203c49fc88bdf
BT_4N2_08 <ktishan3135@bths.edu>
2022-09-30 11:47:24 -0400

    finishing Assignment

--- src/U2L3Runner.java
+++ src/U2L3Runner.java
@@ -1,40 +1,18 @@
+import java.util.Scanner;
+
 public class U2L3Runner {
     public static void main(String[] args)
     {
-        Student student1 = new Student("Charles", "Smith", 2021);
-        student1.addTestScore(85.5);
-        student1.printStudentInfo();
-        System.out.println();
+        Scanner s = new Scanner(System.in);
+        ChatBot chatBot = new ChatBot();
+        chatBot.greeting();
+        System.out.print("What would you like to do?  Enter “i” for info, “w” for weather, help for help ");
+        String answer = s.nextLine();
+        if (answer == "w") {
+            chatBot.weather();
+        }
+        else if (answer == "i") {
 
-        student1.addTestScore(94.5);
-        student1.printStudentInfo();
-        System.out.println();
-
-        student1.addTestScore(100);
-        student1.printStudentInfo();
-        System.out.println();
-
-        student1.addTestScore(96);
-        student1.printStudentInfo();
-        System.out.println();
-
-        Student student2 = new Student("Amy", "Adams", 2020);
-        student2.addTestScore(98.2);
-        student2.printStudentInfo();
-        System.out.println();
-
-        student2.addTestScore(92.5);
-        student2.printStudentInfo();
-        System.out.println();
-
-        double student1avg = student1.averageTestScore();
-        double student2avg = student2.averageTestScore();
-
-        boolean student1greater = (student1avg > student2avg);
-        System.out.println(student1greater);
-        boolean student2greater = (student1avg < student2avg);
-        System.out.println(student2greater);
+        }
     }
-
-
 }
